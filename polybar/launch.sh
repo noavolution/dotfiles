ea15f664a851d2ea3f5af47013925c1792c38383
Noah Janke <69006311+noavolution@users.noreply.github.com>
2021-02-13 00:20:31 +0000

    Update launch.sh

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,16 +3,7 @@
 killall -q polybar
 killall volctl
 killall nm-applet
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar -r main &
 polybar -r second &
 volctl &
 nm-applet &
-
-#if type "xrandr"; then
-  #for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    #MONITOR=$m polybar --reload main &
-  #done
-#else
-  #polybar --reload main &
-#fi
